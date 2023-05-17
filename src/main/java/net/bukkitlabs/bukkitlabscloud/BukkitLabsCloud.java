cdef522776041b9ee16c4ccaeccffdc2273345d5
Asedem <asedem.games@gmail.com>
2023-05-17 15:53:43 +0200

    Implemented encodings

--- src/main/java/net/bukkitlabs/bukkitlabscloud/BukkitLabsCloud.java
+++ src/main/java/net/bukkitlabs/bukkitlabscloud/BukkitLabsCloud.java
@@ -1,6 +1,7 @@
 package net.bukkitlabs.bukkitlabscloud;
 
 public class BukkitLabsCloud {
+
     public static void main(String[] args) {
         System.out.println("BukkitLabsCloud - Starting...");
     }
