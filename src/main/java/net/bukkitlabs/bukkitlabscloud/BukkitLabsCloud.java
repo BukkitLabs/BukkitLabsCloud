7cf97c5e43e0a3d6cfee7274dd71fe369762eab5
finnplsm <finn.plassmeier@plassy.de>
2023-05-17 17:34:54 +0200

    created Logger Class, created ConsoleColor enum

--- src/main/java/net/bukkitlabs/bukkitlabscloud/BukkitLabsCloud.java
+++ src/main/java/net/bukkitlabs/bukkitlabscloud/BukkitLabsCloud.java
@@ -1,8 +1,20 @@
 package net.bukkitlabs.bukkitlabscloud;
 
+import net.bukkitlabs.bukkitlabscloud.logger.Logger;
+
 public class BukkitLabsCloud {
 
+    private final Logger logger;
+
     public static void main(String[] args) {
-        System.out.println("BukkitLabsCloud - Starting...");
+        new BukkitLabsCloud();
+    }
+
+    private BukkitLabsCloud(){
+        this.logger=new Logger();
+        logger.log(Logger.Level.INFO,"Starting BukkitLabsCloud...");
+    }
+    public Logger getLogger(){
+        return logger;
     }
 }
