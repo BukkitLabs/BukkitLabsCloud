19d750ead2119ece2357198fb71eefed31461c2f
Asedem <asedem.games@gmail.com>
2023-05-17 20:06:48 +0200

    Implemented Priority

--- src/main/java/net/bukkitlabs/bukkitlabscloud/util/event/EventHandler.java
+++ src/main/java/net/bukkitlabs/bukkitlabscloud/util/event/EventHandler.java
@@ -4,50 +4,55 @@ import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EventHandler {
 
-    private final List<Listener> listenerList = new LinkedList<>();
+    private final Map<Method, Listener> methodeList = new HashMap<>();
 
     public void registerListener(@NotNull final Listener listener) {
-        listenerList.add(listener);
+        for (final Method method : listener.getClass().getDeclaredMethods()) {
+            if (!method.isAnnotationPresent(Callable.class) ||
+                    method.getParameterTypes().length != 1) continue;
+            methodeList.put(method, listener);
+        }
     }
 
     public void call(@NotNull final Event event) throws EventCannotBeProcessedException {
+        final Method[] methods = this.methodeList.keySet().stream()
+                .filter(method -> method.getParameterTypes()[0].equals(event.getClass()))
+                .sorted((methode1, methode2) -> methode2.getAnnotation(Callable.class).priority().getSlot() -
+                        methode1.getAnnotation(Callable.class).priority().getSlot())
+                .toArray(Method[]::new);
         if (event instanceof Cancelable cancelable) {
-            this.cancelableCall(cancelable);
+            this.cancelableCall(cancelable, methods);
             return;
         }
-        for (Listener listener : listenerList) {
-            for (final Method method : listener.getClass().getDeclaredMethods()) {
-                if (!method.isAnnotationPresent(Callable.class) ||
-                        method.getParameterTypes().length != 1 ||
-                        !method.getParameterTypes()[0].equals(event.getClass())) continue;
-                try {
-                    method.invoke(listener, event);
-                } catch (IllegalAccessException | InvocationTargetException exception) {
-                    throw new EventCannotBeProcessedException(event);
-                }
+        for (final Method method : methods) {
+            if (!method.isAnnotationPresent(Callable.class) ||
+                    method.getParameterTypes().length != 1 ||
+                    !method.getParameterTypes()[0].equals(event.getClass())) continue;
+            try {
+                method.invoke(this.methodeList.get(method), event);
+            } catch (IllegalAccessException | InvocationTargetException exception) {
+                throw new EventCannotBeProcessedException(event);
             }
         }
     }
 
-    private void cancelableCall(final Cancelable event) throws EventCannotBeProcessedException {
+    private void cancelableCall(final @NotNull Cancelable event, final @NotNull Method[] methods) throws EventCannotBeProcessedException {
         boolean canceled = false;
-        for (Listener listener : listenerList) {
-            for (final Method method : listener.getClass().getDeclaredMethods()) {
-                if (!method.isAnnotationPresent(Callable.class) ||
-                        method.getParameterTypes().length != 1 ||
-                        !method.getParameterTypes()[0].equals(event.getClass()) ||
-                        (canceled && !method.getAnnotation(Callable.class).ignoreCancelled())) continue;
-                try {
-                    method.invoke(listener, event);
-                    if (event.isCanceled()) canceled = true;
-                } catch (IllegalAccessException | InvocationTargetException exception) {
-                    throw new EventCannotBeProcessedException((Event) event);
-                }
+        for (final Method method : methods) {
+            if (!method.isAnnotationPresent(Callable.class) ||
+                    method.getParameterTypes().length != 1 ||
+                    !method.getParameterTypes()[0].equals(event.getClass()) ||
+                    (canceled && !method.getAnnotation(Callable.class).ignoreCancelled())) continue;
+            try {
+                method.invoke(this.methodeList.get(method), event);
+                if (event.isCanceled()) canceled = true;
+            } catch (IllegalAccessException | InvocationTargetException exception) {
+                throw new EventCannotBeProcessedException((Event) event);
             }
         }
     }
