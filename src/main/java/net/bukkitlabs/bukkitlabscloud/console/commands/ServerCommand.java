1ebd2435b74a695d799fc2902b3c8e8d6b726905
finnplsm <finn.plassmeier@plassy.de>
2023-05-18 13:12:50 +0200

    added ServerCommand

---
+++ src/main/java/net/bukkitlabs/bukkitlabscloud/console/commands/ServerCommand.java
@@ -0,0 +1,62 @@
+package net.bukkitlabs.bukkitlabscloud.console.commands;
+
+import net.bukkitlabs.bukkitlabscloud.BukkitLabsCloud;
+import net.bukkitlabs.bukkitlabscloud.console.CloudCommand;
+import net.bukkitlabs.bukkitlabscloud.console.Command;
+import net.bukkitlabs.bukkitlabscloud.console.Logger;
+
+public class ServerCommand extends CloudCommand{
+    public ServerCommand(){
+        super("server","Usage: server [start|stop|restart|create] [{name}] [spigot|bukkit|paper] [{version}]");
+    }
+
+
+    @Override
+    public boolean onCommand(Command command,String commandLabel,String[] args){
+        if(args[0]!=null&&args[1]!=null){
+            final String option=args[0];
+            final String name=args[1];
+            switch(option){
+                case "start":
+                    // TODO: TabCompletion for name
+                    // TODO: api.startServer(name) resturns a Log (example: FINE:"Server name started" or ERROR:"Server name could not start + exception")
+                    // Log log=api.startServer(name);
+                    // BukkitLabsCloud.getLogger().log(log);
+                    break;
+                case "stop":
+                    // TODO: TabCompletion for name
+                    // TODO: api.stopServer(name) resturns a Log (example: FINE:"Server name stopped" or ERROR:"Server name could not stop + exception")
+                    // Log log=api.stop(name);
+                    // BukkitLabsCloud.getLogger().log(log);
+                    break;
+                case "restart":
+                    // TODO: TabCompletion for name
+                    // TODO: api.restartServer(name) resturns a Log (example: FINE:"Server name is restarting" or ERROR:"Server name could not restart + exception")
+                    // Log log=api.restart(name);
+                    // BukkitLabsCloud.getLogger().log(log);
+                    break;
+                case "create":
+                    if(args[2]!=null&&args[3]!=null){
+                        final String api=args[2];
+                        final String version=args[3];
+                        // TODO: TabCompletion for name
+                        // TODO: TabCompletion for api
+                        // TODO: TabCompletion for version
+                        // TODO: api.createServer(name,api,version) resturns a Log (example: FINE:"Server name is creating" or ERROR:"Server name could not creat + exception")
+                    }else {
+
+                    }
+                    break;
+                default:
+                    BukkitLabsCloud.getLogger().log(Logger.Level.ERROR, "Invalid arguement!");
+                    break;
+
+            }
+        }else {
+
+        }
+
+        return false;
+    }
+
+}
